981260948e55494fc0aa69ba58146d05b4ffe969
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-05-05 13:50:04 -0500

    done

--- src/main.js
+++ src/main.js
@@ -14,3 +14,20 @@
 function addBinary(a,b) {
     return (a+b).toString(2)
   }
+
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example:
+// 348597 => [7,9,5,8,4,3]
+// 0 => [0]
+
+function digitize(n) {
+  return String(n).split('').map(Number).reverse()
+}
+
+// SOLUTION EXPLAINED
+// String() converts the input to a string
+// .split("") splits the strings characters with commas and places them into an array
+// .map(Number) maps over each item and reverts them back to a number
+// .reverse() reverses the array
