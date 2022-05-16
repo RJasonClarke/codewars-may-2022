b93665b2bf0ab1dfc3804a0660a870675e1105a4
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-05-16 10:59:48 -0500

    done

--- src/main.js
+++ src/main.js
@@ -68,3 +68,12 @@ function squareDigits(num){
 function squareDigits(num){
   return Number(('' + num).split('').map(function (val) { return val * val;}).join(''));
 }
+
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+  return str.split("").reverse().join("").split(" ").reverse().join(" ");
+}
