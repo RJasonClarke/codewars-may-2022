5f574e5ae97c53b286b92f5eb5c86b65ec2226aa
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-05-27 11:30:06 -0500

    done

--- src/main.js
+++ src/main.js
@@ -172,3 +172,22 @@ function litres(time) {
 function findDifference(a, b) {
   return Math.abs(a.reduce((a, b) => a * b) - b.reduce((a, b) => a * b))
 }
+
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+// Notes:
+
+// If either input is an empty string, consider it as zero.
+
+// Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+function sumStr(a,b) {
+  return String(Number(a) + Number(b))
+}
