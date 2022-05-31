2da460047c9b92885d673a46652b4264608793d5
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-05-31 09:01:37 -0500

    done

--- src/main.js
+++ src/main.js
@@ -210,3 +210,15 @@ function sumStr(a,b) {
 function checkForFactor (base, factor) {
   return base % factor === 0 ? true : false
 }
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+
+function paperwork(n, m) {
+  return Math.max(n, 0) * Math.max(m, 0)
+}
